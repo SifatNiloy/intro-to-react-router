ecc475cb44ede76c836b7dadc18dedf7857580c8
SifatNiloy <sifatniloy18@gmail.com>
2023-08-24 13:21:26 +0600

    nested route created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import About from '../About/About';
-import Contact from '../Contact/Contact';
+
 
 const Header = () => {
     return (
         <div>
             <a href="/">Home</a>
-            <a href="/about"><About/></a>
-            <a href="/contact"><Contact/></a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <h1>This is home</h1>
+      <Outlet />
     </div>
   );
 };
